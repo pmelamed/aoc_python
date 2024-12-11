b6804dea44716102aa916a8e73fd55d0205fe013
pavel.melamed <pavel.melamed@form.com>
2024-12-11 14:32:19 +0100

    Day 24'11 - task 2 dynamic solution

--- day24_11.py
+++ day24_11.py
@@ -1,16 +1,17 @@
-import helper
-from helper import exec_tasks, read_file, print_ex
+import collections
+
+from helper import exec_task, exec_tasks, print_ex
 
 
 class Data:
-    stones: list[ str ]
+    stones: list[ int ]
 
-    def __init__( self, stones: list[ str ] ):
+    def __init__( self, stones: list[ int ] ):
         self.stones = stones
 
 
 def prepare( lines: list[ str ] ) -> Data:
-    return Data( lines[ 0 ].split( " " ) )
+    return Data( [ int( s ) for s in lines[ 0 ].split( " " ) ] )
 
 
 def task1( data: Data ) -> int:
@@ -22,21 +23,28 @@ def task2( data: Data ) -> int:
 
 
 def process_stones( data, max_steps ):
-    return sum( process_stone( stone, 0, max_steps ) for stone in data.stones )
-
-
-def process_stone( stone: str, steps: int, max_steps: int ) -> int:
-    if steps == max_steps: return 1
-    if stone == "0": return process_stone( "1", steps + 1, max_steps )
-    if len(stone) > 1 and len( stone ) % 2 == 0:
-        return process_stone( str( int( stone[ : len( stone ) // 2 ] ) ), steps + 1, max_steps ) + \
-            process_stone( str( int( stone[ len( stone ) // 2: ] ) ), steps + 1, max_steps )
-    return process_stone( str( int( stone ) * 2024 ), steps + 1, max_steps )
+    step: dict[ int, int ] = dict( zip( data.stones, len( data.stones ) * [ 1 ] ) )
+    for _ in range( max_steps ):
+        next_step = collections.defaultdict( lambda: 0 )
+        for stone_value in step.keys():
+            stones_count = step[ stone_value ]
+            if stone_value == 0:
+                next_step[ 1 ] += stones_count
+            else:
+                stone_str = str( stone_value )
+                stone_len = len( stone_str )
+                if stone_len % 2 == 0:
+                    next_step[ int( stone_str[ : stone_len // 2 ] ) ] += stones_count
+                    next_step[ int( stone_str[ stone_len // 2: ] ) ] += stones_count
+                else:
+                    next_step[ stone_value * 2024 ] += stones_count
+        step = next_step
+    return sum( step.values() )
 
 
 def main():
-    helper.exec_task( prepare, task1,  [ "125 17" ], 55312 )
-    exec_tasks( prepare, task1, task2, [ "4189 413 82070 61 655813 7478611 0 8" ], 186203, None )
+    exec_task( prepare, task1, [ "125 17" ], 55312 )
+    exec_tasks( prepare, task1, task2, [ "4189 413 82070 61 655813 7478611 0 8" ], 186203, 221291560078593 )
 
 
 if __name__ == '__main__':
