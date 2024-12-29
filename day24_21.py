098541b11c45e2e4ccc7ba65203adbec20fa739c
pavel.melamed <pavel.melamed@form.com>
2024-12-29 02:47:47 +0100

    Day 24'21 - task 2

--- day24_21.py
+++ day24_21.py
@@ -2,9 +2,9 @@ from collections import deque
 from itertools import pairwise, product
 from typing import Optional
 
-from helper import Coord, exec_tasks, Field, field_value, move_forward, print_ex, read_file
+from helper import Coord, exec_task, exec_tasks, Field, field_value, inside_rect, move_forward, print_ex, read_file
 
-type KeyPadMoves = dict[ tuple[ str, str ], str ]
+type KeyPadMoves = dict[ tuple[ str, str ], int ]
 type KeyPadLayout = dict[ str, Coord ]
 
 NUM_KEYPAD_LAYOUT: KeyPadLayout = {
@@ -33,7 +33,7 @@ CTRL_KEYPAD_LAYOUT: KeyPadLayout = {
 CTRL_KEYPAD_SIZE = (3, 2)
 CTRL_KEYPAD_GAP = (0, 0)
 
-HUMAN_KEYPAD_MOVES: KeyPadMoves = dict( ((a, b), b) for a, b in product( "^<v>A", repeat = 2 ) )
+HUMAN_KEYPAD_MOVES: KeyPadMoves = dict( ((a, b), 1) for a, b in product( "^<v>A", repeat = 2 ) )
 
 DIR_CHARS = {
     "<": (-1, 0),
@@ -44,38 +44,23 @@ DIR_CHARS = {
 
 
 def task1( codes: list[ str ] ) -> int:
-    human_moves = get_optimal_moves( CTRL_KEYPAD_LAYOUT, CTRL_KEYPAD_SIZE, CTRL_KEYPAD_GAP, HUMAN_KEYPAD_MOVES )
-    # print( f" Human = {human_moves}" )
-    robot1_moves = get_optimal_moves( CTRL_KEYPAD_LAYOUT, CTRL_KEYPAD_SIZE, CTRL_KEYPAD_GAP, human_moves )
-    # print( f" Robot #1 = {robot1_moves}" )
-    robot2_moves = get_optimal_moves( NUM_KEYPAD_LAYOUT, NUM_KEYPAD_SIZE, NUM_KEYPAD_GAP, robot1_moves )
-    # print( f" Robot #3 = {robot2_moves}" )
-    # results = list( (get_code_value( code ),
-    #                  get_press_seq( code, robot2_moves ),
-    #                  len( get_press_seq( code, robot2_moves ) ))
-    #                 for code in codes )
-    # print( results )
-    # for _1, seq, _2 in results:
-    #     print( seq )
-    #     seq = emulate( seq, CTRL_KEYPAD_LAYOUT )
-    #     print( seq )
-    #     seq = emulate( seq, CTRL_KEYPAD_LAYOUT )
-    #     print( seq )
-    #     seq = emulate( seq, NUM_KEYPAD_LAYOUT )
-    #     print( seq )
+    moves = HUMAN_KEYPAD_MOVES
+    for _ in range( 2 ):
+        moves = get_optimal_moves( CTRL_KEYPAD_LAYOUT, CTRL_KEYPAD_SIZE, CTRL_KEYPAD_GAP, moves )
+    moves = get_optimal_moves( NUM_KEYPAD_LAYOUT, NUM_KEYPAD_SIZE, NUM_KEYPAD_GAP, moves )
     return sum(
-        len( get_press_seq( code, robot2_moves ) ) * get_code_value( code )
+        get_press_seq( code, moves ) * get_code_value( code )
         for code in codes
     )
 
 
 def task2( codes: list[ str ] ) -> int:
     moves = HUMAN_KEYPAD_MOVES
-    for _ in range(25):
+    for _ in range( 25 ):
         moves = get_optimal_moves( CTRL_KEYPAD_LAYOUT, CTRL_KEYPAD_SIZE, CTRL_KEYPAD_GAP, moves )
     moves = get_optimal_moves( NUM_KEYPAD_LAYOUT, NUM_KEYPAD_SIZE, NUM_KEYPAD_GAP, moves )
     return sum(
-        len( get_press_seq( code, moves ) ) * get_code_value( code )
+        get_press_seq( code, moves ) * get_code_value( code )
         for code in codes
     )
 
@@ -84,8 +69,8 @@ def get_code_value( code: str ) -> int:
     return int( code[ :-1 ], 10 )
 
 
-def get_press_seq( code: str, keypad: KeyPadMoves ) -> str:
-    return "".join( keypad[ (a, b) ] for a, b in pairwise( "A" + code ) )
+def get_press_seq( code: str, keypad: KeyPadMoves ) -> int:
+    return sum( keypad[ (a, b) ] for a, b in pairwise( "A" + code ) )
 
 
 def get_optimal_moves( dst_layout: KeyPadLayout,
@@ -96,49 +81,37 @@ def get_optimal_moves( dst_layout: KeyPadLayout,
                  for a, b in product( dst_layout.keys(), repeat = 2 ) )
 
 
-def get_optimal_way( a: Coord, b: Coord, size: Coord, gap: Optional[ Coord ], moves: KeyPadMoves ) -> str:
-    max_way_length = size[ 0 ] * size[ 1 ] * max( len( s ) for s in moves.values() ) + 1
-    min_way = None
+def get_optimal_way( a: Coord, b: Coord, size: Coord, gap: Optional[ Coord ], moves: KeyPadMoves ) -> int:
+    max_way_length = size[ 0 ] * size[ 1 ] * max( s for s in moves.values() ) + 1
     min_way_len = max_way_length
-    passed: Field[ int ] = field_value( size[ 0 ], size[ 1 ], max_way_length )
-    if gap is not None: passed[ gap ] = -1
-    wave = deque[ tuple[ Coord, str, str ] ]()
-    wave.append( (a, "", "A") )
+    field_rect = (0, 0, size[ 0 ], size[ 1 ])
+    passed: dict[ str, Field[ int ] ] = dict( (key, field_value( size[ 0 ], size[ 1 ], max_way_length ))
+                                              for key in DIR_CHARS )
+    passed["A"] = field_value( size[ 0 ], size[ 1 ], max_way_length )
+    if gap is not None:
+        for key in passed: passed[ key ][ gap ] = -1
+    wave = deque[ tuple[ Coord, int, str ] ]()
+    wave.append( (a, 0, "A") )
     while wave:
-        pt, way, last_dir = wave.popleft()
-        if pt not in passed or passed[ pt ] < len( way ): continue
+        pt, length, last_dir = wave.popleft()
+        if not inside_rect( pt, field_rect ) or passed[ last_dir ][ pt ] < length: continue
         if pt == b:
-            full_way = way + moves[ (last_dir, "A") ]
-            full_way_len = len( full_way )
+            full_way_len = length + moves[ (last_dir, "A") ]
             if full_way_len < min_way_len:
-                min_way = full_way
                 min_way_len = full_way_len
             continue
-        passed[ pt ] = len( way )
-        wave.extend( (move_forward( pt, d_vector ), way + moves[ (last_dir, d_char) ], d_char)
+        passed[ last_dir ][ pt ] = length
+        wave.extend( (move_forward( pt, d_vector ), length + moves[ (last_dir, d_char) ], d_char)
                      for d_char, d_vector in DIR_CHARS.items() )
-    return min_way
-
-
-def emulate( moves: str, layout: KeyPadLayout ) -> str:
-    pt = layout[ "A" ]
-    result = ""
-    for move in moves:
-        if move == "A":
-            result += next( key for key in layout if layout[ key ] == pt )
-        else:
-            pt = move_forward( pt, DIR_CHARS[ move ] )
-    return result
+    return min_way_len
 
 
 def main():
-    exec_tasks(
+    exec_task(
         None,
         task1,
-        task2,
         read_file( 'data/day24_21.sample' ),
-        126384,
-        None
+        126384
     )
     exec_tasks(
         None,
@@ -146,7 +119,7 @@ def main():
         task2,
         read_file( 'data/day24_21.in' ),
         176870,
-        None
+        223902935165512
     )
 
 
