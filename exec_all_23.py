957707da3717e47600bc48e9cdd55b3dc0ace2d1
pavel.melamed <pavel.melamed@form.com>
2025-01-06 11:23:37 +0100

    Exec All for Year 23

---
+++ exec_all_23.py
@@ -0,0 +1,9 @@
+import day23_20
+import helper
+
+if __name__ == '__main__':
+    try:
+        print( "-- 23' 20 ----------------------------------------------" )
+        day23_20.main()
+    except Exception as ex:
+        helper.print_ex( ex )
