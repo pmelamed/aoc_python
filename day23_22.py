e5bcc5a455aa1c0bd2963e2651ed0c98136c67aa
pavel.melamed <pavel.melamed@form.com>
2025-01-07 14:53:34 +0100

    Day 2023-22 task 2

--- day23_22.py
+++ day23_22.py
@@ -13,44 +13,51 @@ type Boxes = list[ Rect3D ]
 CellInfo = namedtuple( "CellInfo", [ "box", "height" ] )
 
 
-def prepare( lines: list[ str ] ) -> Boxes:
-    result = sorted( [ parse_box( line ) for line in lines ], key = lambda b: b.a.z )
-    return result
-
-
-def task1( boxes: Boxes ) -> int:
+class Data:
+    boxes: Boxes
     supports: list[ set[ int ] ] = list()
     supported: list[ set[ int ] ] = list()
-    width = max( box.b.x for box in boxes ) + 1
-    height = max( box.b.y for box in boxes ) + 1
-    top_view: helper.Field[ CellInfo ] = field_value( width, height, CellInfo( box = -1, height = 0 ) )
-    for index, box in enumerate( boxes ):
-        max_height = max( top_view[ cell ].height
-                          for cell in product( range( box.a.x, box.b.x + 1 ),
-                                               range( box.a.y, box.b.y + 1 ) ) )
-        # print( f"{chr( ord( 'A' ) + index )} = {box}" )
-        # print( top_view.dump( lambda _x, _y, cell: f"[{chr( ord( 'A' ) + cell.box )},{cell.height:3}]" ) )
-        new_height = max_height + box.b.z - box.a.z + 1
-        supports.append( set() )
-        supported.append( set() )
-        for cell_coords in product( range( box.a.x, box.b.x + 1 ), range( box.a.y, box.b.y + 1 ) ):
-            cell = top_view[ cell_coords ]
-            if cell.height == max_height and cell.box >= 0:
-                supports[ cell.box ].add( index )
-                supported[ index ].add( cell.box )
-            top_view[ cell_coords ] = CellInfo( box = index, height = new_height )
-    # for box_index in range( len( boxes ) ):
-    #     print( f"Box {chr( ord( 'A' ) + box_index )}" )
-    #     print( f"  Supports {[ chr( ord( 'A' ) + i ) for i in supports[ box_index ] ]}" )
-    #     print( f"  Supported by {[ chr( ord( 'A' ) + i ) for i in supported[ box_index ] ]}" )
-    disintegratable = [ box_index for box_index in range( len( boxes ) ) if
-                        all_supported( supports[ box_index ], supported ) ]
-    # print( [ chr( ord( 'A' ) + i ) for i in disintegratable ] )
-    return len( disintegratable )
+
+    def __init__( self, lines: list[ str ] ):
+        boxes = sorted( [ parse_box( line ) for line in lines ], key = lambda b: b.a.z )
+        supports: list[ set[ int ] ] = list()
+        supported: list[ set[ int ] ] = list()
+        width = max( box.b.x for box in boxes ) + 1
+        height = max( box.b.y for box in boxes ) + 1
+        top_view: helper.Field[ CellInfo ] = field_value( width, height, CellInfo( box = -1, height = 0 ) )
+        for index, box in enumerate( boxes ):
+            max_height = max( top_view[ cell ].height
+                              for cell in product( range( box.a.x, box.b.x + 1 ),
+                                                   range( box.a.y, box.b.y + 1 ) ) )
+            new_height = max_height + box.b.z - box.a.z + 1
+            supports.append( set() )
+            supported.append( set() )
+            for cell_coords in product( range( box.a.x, box.b.x + 1 ), range( box.a.y, box.b.y + 1 ) ):
+                cell = top_view[ cell_coords ]
+                if cell.height == max_height and cell.box >= 0:
+                    supports[ cell.box ].add( index )
+                    supported[ index ].add( cell.box )
+                top_view[ cell_coords ] = CellInfo( box = index, height = new_height )
+        self.boxes = boxes
+        self.supports = supports
+        self.supported = supported
 
 
-def task2( boxes: Boxes ) -> int:
-    return 0
+def task1( data: Data ) -> int:
+    return sum( 1 for box_index in range( len( data.boxes ) ) if
+                all_supported( data.supports[ box_index ], data.supported ) )
+
+
+def task2( data: Data ) -> int:
+    result = 0
+    for box_index in range( len( data.boxes ) ):
+        fallen: set[ int ] = { box_index }
+        for supported_index in range( box_index + 1, len( data.boxes ) ):
+            if (len( data.supported[ supported_index ] & fallen ) != 0
+                    and len( data.supported[ supported_index ] - fallen ) == 0):
+                fallen.add( supported_index )
+        result += len( fallen ) - 1
+    return result
 
 
 def parse_box( line: str ) -> Rect3D:
@@ -63,18 +70,18 @@ def all_supported( supported_boxes: Iterable[ int ], supported_by: list[ set[ in
 
 
 def main():
-    helper.exec_tasks( prepare,
+    helper.exec_tasks( Data,
                        task1,
                        task2,
                        helper.read_file( 'data/day23_22.sample' ),
                        5,
-                       None )
-    helper.exec_tasks( prepare,
+                       7 )
+    helper.exec_tasks( Data,
                        task1,
                        task2,
                        helper.read_file( 'data/day23_22.in' ),
-                       None,
-                       None )
+                       389,
+                       70609 )
 
 
 if __name__ == '__main__':
--- exec_all_23.py
+++ exec_all_23.py
@@ -1,9 +1,11 @@
 import day23_20
+import day23_22
 import helper
 
 if __name__ == '__main__':
     try:
         print( "-- 23' 20 ----------------------------------------------" )
         day23_20.main()
+        day23_22.main()
     except Exception as ex:
         helper.print_ex( ex )
