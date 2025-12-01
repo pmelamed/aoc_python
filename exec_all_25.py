054796c819095d8e762b8b908cbf1ccf6778c76c
Pavlo Melamed <pavel.melamed@form.com>
2025-12-01 12:34:54 +0100

    Day 25'01

---
+++ exec_all_25.py
@@ -0,0 +1,9 @@
+import day25_01
+import helper
+
+if __name__ == '__main__':
+    try:
+        print( "-- 25' 01 ----------------------------------------------" )
+        day25_01.main()
+    except Exception as ex:
+        helper.print_ex( ex )
