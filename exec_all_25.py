c2f642d0fc99c22814e5364ce112f9504a11a38d
Pavlo Melamed <pavel.melamed@form.com>
2025-12-02 17:53:56 +0100

    Day 25'02

--- exec_all_25.py
+++ exec_all_25.py
@@ -1,9 +1,12 @@
 import day25_01
+import day25_02
 import helper
 
 if __name__ == '__main__':
     try:
         print( "-- 25' 01 ----------------------------------------------" )
         day25_01.main()
+        print( "-- 25' 02 ----------------------------------------------" )
+        day25_02.main()
     except Exception as ex:
         helper.print_ex( ex )
